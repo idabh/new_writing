9a0dba8ef629dc501ce6f468e17c4174bd6080e2
Pascale Feldkamp Moreira <pascale.f.herold@hotmail.com>
2024-11-21 12:07:11 +0100

    update reqs

--- app.py
+++ app.py
@@ -32,11 +32,25 @@ import os
 #     except:
 #         pass
 
-nltk.download('punkt')
-nltk.download('vader_lexicon')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('universal_tagset')
-nltk.download('stopwords')
+import os
+import nltk
+
+nltk_data_dir = "./resources/nltk_data_dir/"
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.clear()
+nltk.data.path.append(nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('vader_lexicon', download_dir=nltk_data_dir)
+nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_dir)
+nltk.download('universal_tagset', download_dir=nltk_data_dir)
+
+# nltk.download('punkt')
+# nltk.download('vader_lexicon')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('universal_tagset')
+# nltk.download('stopwords')
 
 # URL of the dataset
 url = 'https://raw.githubusercontent.com/seantrott/cs_norms/refs/heads/main/data/lexical/lancaster_norms.csv'
