ecf681ca4b7fecdf17847ffda91e48e01899b856
Pascale Feldkamp Moreira <pascale.f.herold@hotmail.com>
2024-11-23 01:46:19 +0100

    add hemingway

--- app.py
+++ app.py
@@ -54,7 +54,8 @@ if st.button("Or: show Hemingway excerpt"):
         hemingway_text = file.read()
         #user_text = hemingway_text
         # also set the text area to the hemingway text
-        st.text_area("Beginning of *The Old Man and the Sea* – copy and paste above", value=hemingway_text, height=300)
+        #st.text_area("Beginning of *The Old Man and the Sea* – copy and paste above", value=hemingway_text, height=300)
+        st.code(hemingway_text, language="python")
 
 # Slider for adjustable parameters
 st.sidebar.header("Adjustable Parameters")
