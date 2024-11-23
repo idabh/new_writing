c90b5e9f755aafb51b6ac494bb5fad65c4303a1f
Pascale Feldkamp Moreira <pascale.f.herold@hotmail.com>
2024-11-23 16:12:04 +0100

    add hapax scatterplot

--- app.py
+++ app.py
@@ -474,7 +474,7 @@ if user_text:
                 # two subplots
                 plt.figure(figsize=(10, 3))
                 plt.subplot(1, 2, 1)
-                sns.barplot(x=[word for word, _ in most_abstract], y=[score for _, score in most_abstract], palette="viridis")
+                sns.barplot(x=[word for word, _ in most_abstract], y=[score for _, score in most_abstract], palette="reds")
                 plt.title("Most Abstract Words")
                 plt.ylabel("Concreteness")
                 # set ylim to 0,5
